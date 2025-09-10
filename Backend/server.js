6d2700c41503c3756fe343a1f747d6f23dcf5550
sankarcodings <sankarcodings@gmail.com>
2025-09-10 22:48:01 +0530

    changed

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,10 @@
 import dotenv from "dotenv";
 
 import app from './app.js'
-import {PORT} from '../Backend/src/config/env.js'
 
 dotenv.config();
 
-const port = PORT || 5000;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port , () => {
